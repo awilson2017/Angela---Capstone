3c5e3a0b98b7505119cba523847ad027a4c81032
Angela Wilson <wilsonange@gmail.com>
2018-01-04 16:04:04 -0800

    finish working on migrations and add a different Paperclip code to conif/nvironments/development.rb

--- db/migrate/20180103212751_create_cards.rb
+++ db/migrate/20180103212751_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[5.1]
     create_table :cards do |t|
       t.integer :deck_id
       t.string :front
+      t.string :back
 
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180104235124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "deck_id"
+    t.string "front"
+    t.string "back"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_url"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
