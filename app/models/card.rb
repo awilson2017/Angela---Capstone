5c6d5de194bafef18f19876d39770b938db37cdc
Angela Wilson <wilsonange@gmail.com>
2018-01-04 16:51:25 -0800

    add production connection between app and S3

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+  has_attached_file :image,
+                    styles: { :medium => "200x200>", :thumb => "100x100>" }
+  validates_attachment_content_type :image, :content_type => /^image\/(png|gif|jpeg|jpg)/
+
 end
