2c112b093c9cfe3f29b9fda207f4b71548eba5ce
Angela Wilson <wilsonange@gmail.com>
2018-01-04 20:53:42 -0800

    write ApiWrapper#self.google_translate method. More work needed to complete the api consumer

--- lib/api_wrapper.rb
+++ lib/api_wrapper.rb
@@ -16,37 +16,37 @@ class ApiWrapper
     check_status(data)
 
     # need array to store the parsed api hash results
-    recipes_list = []
+    translations_list = []
 
-    if data["hits"]
-      # data["hits"] is an array of hashes, within each hash there are sub-hashes and sub-arrays, we need
-      data["hits"].each do |recipe_info_hash|
-        recipes_list << self.create_recipe(recipe_info_hash["recipe"])
+    if data["translations"]
+      # data["translations"] is an array of hashes, within each hash there are sub-hashes and sub-arrays, we need
+      data["translations"].each do |translation_info_hash|
+        translations_list << self.create_translation(translation_info_hash["translatedText"])
       end
     end
-    return recipes_list
+    return translations_list
   end
 
-  def self.find_recipe(uri)
-    # make a url to call on the edamam api
-    recipe_link = BASE_URL + "?r=#{uri}&app_id=#{API_ID}&app_key=#{TOKEN}"
-
-    # https://api.edamam.com/search
-    # https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free
-
-    data = HTTParty.get(recipe_link)
-
-
-
-
-    # parse the JSON data in order to get recipe details for the show page
-    if data.empty?
-      raise ApiError.new("No recipe details for this link")
-    else
-      recipe_detail = self.create_recipe(data[0])
-    end
-    return recipe_detail
-  end
+  # def self.find_recipe(uri)
+  #   # make a url to call on the edamam api
+  #   recipe_link = BASE_URL + "?r=#{uri}&app_id=#{API_ID}&app_key=#{TOKEN}"
+  #
+  #   # https://api.edamam.com/search
+  #   # https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free
+  #
+  #   data = HTTParty.get(recipe_link)
+  #
+  #
+  #
+  #
+  #   # parse the JSON data in order to get recipe details for the show page
+  #   if data.empty?
+  #     raise ApiError.new("No recipe details for this link")
+  #   else
+  #     recipe_detail = self.create_recipe(data[0])
+  #   end
+  #   return recipe_detail
+  # end
 
   private
 
@@ -57,21 +57,21 @@ class ApiWrapper
   end
 
 
-  def self.create_recipe(recipe_hash)
-    recipe = Recipe.new(
-
-      # gets the name of the recipe
-      recipe_hash["label"],
-      recipe_hash["url"],
-      recipe_hash["ingredientLines"],
-      recipe_hash["dietLabels"],
-      recipe_hash["uri"],
-      recipe_hash["source"],
-      {
-        image: recipe_hash["image"],
-      }
-    )
-    return recipe
-  end
+  # def self.create_recipe(recipe_hash)
+  #   recipe = Recipe.new(
+  #
+  #     # gets the name of the recipe
+  #     recipe_hash["label"],
+  #     recipe_hash["url"],
+  #     recipe_hash["ingredientLines"],
+  #     recipe_hash["dietLabels"],
+  #     recipe_hash["uri"],
+  #     recipe_hash["source"],
+  #     {
+  #       image: recipe_hash["image"],
+  #     }
+  #   )
+  #   return recipe
+  # end
 
 end
